6e58d4f7c27b26aa34e3db4fec8662bc80ee4be3
Tomosom <764575907@qq.com>
2020-05-24 22:01:38 +0800

    add 033 note

--- qt_projects/code/033_Text_and_data_streams/code/33-1/main.cpp
+++ qt_projects/code/033_Text_and_data_streams/code/33-1/main.cpp
@@ -5,15 +5,18 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QFile file("C:/Users/hp/Desktop/test.hex");
+    //QFile file("C:/Users/hp/Desktop/test.hex");
+    QFile file("test.hex");
 
+    /* 没有加 QIODevice::Text 表示以字节流的方式打开文件 */
     if( file.open(QIODevice::WriteOnly) )
     {
         QString dt = "D.T.Software";
         double value = 3.14;
 
-        file.write(dt.toStdString().c_str());
-        file.write(reinterpret_cast<char*>(&value), sizeof(value));
+        /* 使用QFile进行数据或文本的读写是可行的,此处为不方便的地方-类型转换 */
+        file.write(dt.toStdString().c_str()); // 将QString数据转为直接的二进制数据
+        file.write(reinterpret_cast<char*>(&value), sizeof(value)); // 将其转换为字节类型的数据
 
         file.close();
     }
@@ -23,7 +26,7 @@ int main(int argc, char *argv[])
         QString dt = "";
         double value = 0;
 
-        dt = QString(file.read(12));
+        dt = QString(file.read(12)); /* "D.T.Software" */
         file.read(reinterpret_cast<char*>(&value), sizeof(value));
 
         file.close();
--- qt_projects/code/033_Text_and_data_streams/code/33-2/main.cpp
+++ qt_projects/code/033_Text_and_data_streams/code/33-2/main.cpp
@@ -38,10 +38,11 @@ void data_stream_test(QString f)
 {
     QFile file(f);
 
-    if( file.open(QIODevice::WriteOnly) )
+    if( file.open(QIODevice::WriteOnly) ) /* 此处没有 QIODevice::Text */
     {
         QDataStream out(&file);
 
+        /* 解决不同Qt版本的数据流文件格式可能不同的问题 */
         out.setVersion(QDataStream::Qt_4_7);
 
         out << QString("D.T.Software");
@@ -51,13 +52,14 @@ void data_stream_test(QString f)
         file.close();
     }
 
-    if( file.open(QIODevice::ReadOnly) )
+    if( file.open(QIODevice::ReadOnly) ) /* 此处没有 QIODevice::Text */
     {
         QDataStream in(&file);
         QString dt = "";
         QString result = "";
         double value = 0;
 
+        /* 解决不同Qt版本的数据流文件格式可能不同的问题 */
         in.setVersion(QDataStream::Qt_4_7);
 
         in >> dt;
@@ -76,8 +78,11 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    text_stream_test("C:/Users/hp/Desktop/text.txt");
-    data_stream_test("C:/Users/hp/Desktop/data.dat");
-    
+    //text_stream_test("C:/Users/hp/Desktop/text.txt");
+    //data_stream_test("C:/Users/hp/Desktop/data.dat");
+
+    text_stream_test("text.txt");
+    data_stream_test("data.dat");
+
     return a.exec();
 }
