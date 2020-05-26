3b2ee0eaa93897998c804933968623088f3fb4ee
Tomosom <764575907@qq.com>
2020-05-27 00:59:20 +0800

    add 035 note

--- qt_projects/code/035_Data_access_in_text_editor/code/NotePad/main.cpp
+++ qt_projects/code/035_Data_access_in_text_editor/code/NotePad/main.cpp
@@ -1,11 +1,15 @@
 #include <QtGui/QApplication>
 #include "MainWindow.h"
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow* w = MainWindow::NewInstance();
     int ret = -1;
+
+    w->setFixedSize(600, 500);
+    w->move((a.desktop()->width() - w->width()) / 2, (a.desktop()->height() - w->height()) / 2);
 
     if( w != NULL )
     {
