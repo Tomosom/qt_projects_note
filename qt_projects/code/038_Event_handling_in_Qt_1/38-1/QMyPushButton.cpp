fb6310872de532a9e4f8cd8383f556c381e73362
Tomosom <764575907@qq.com>
2020-06-17 23:38:05 +0800

    add 038 note

--- qt_projects/code/038_Event_handling_in_Qt_1/38-1/QMyPushButton.cpp
+++ qt_projects/code/038_Event_handling_in_Qt_1/38-1/QMyPushButton.cpp
@@ -6,18 +6,19 @@ QMyPushButton::QMyPushButton(QWidget* parent, QButtonListener* listener) : QPush
     m_listener = listener;
 }
 
-void QMyPushButton::mouseReleaseEvent(QMouseEvent *e)
+void QMyPushButton::mouseReleaseEvent(QMouseEvent *e) /* 调用重写的函数 */
 {
     if( m_listener != NULL )
     {
-        m_listener(this, e);
+        m_listener(this, e); /* 调用函数指针 */
 
-        e->accept();
-
-        setDown(false);
+        /* 更新界面相关的逻辑, 若无下面语句,按钮按下去后不能弹起来 */
+        e->accept(); /* 标记当前的事件已经被处理了 */
+        setDown(false); /* 处理UI的更新, false : 使按钮弹起来 */
     }
     else
     {
+        /* 默认的QPushButton的行为 */
         QPushButton::mouseReleaseEvent(e);
     }
 }
