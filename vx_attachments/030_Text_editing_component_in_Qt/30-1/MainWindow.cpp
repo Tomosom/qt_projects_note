7c3cc74b5f20af5e37e2664a46c41bd234a43ee8
Tomosom <764575907@qq.com>
2021-12-23 19:11:32 +0800

    add 030

--- vx_attachments/030_Text_editing_component_in_Qt/30-1/MainWindow.cpp
+++ vx_attachments/030_Text_editing_component_in_Qt/30-1/MainWindow.cpp
@@ -5,24 +5,32 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
 {
     resize(600, 420);
 
+    /* QLineEdit */
     lineEdit.move(20, 20);
     lineEdit.resize(560, 100);
     lineEdit.insert("QLineEdit");
-    lineEdit.insert("\n");
-    lineEdit.insert("<img src=\"C:\\Users\\hp\\Desktop\\D.T.png\" />");
+    lineEdit.insert("\n"); /* 换行无作用 */
+    lineEdit.insert("<img src=\":/res/D.T.png\" />");
 
+    /* QPlainTextEdit */
     plainEdit.move(20, 130);
     plainEdit.resize(560, 130);
     plainEdit.insertPlainText("QPlainTextEdit");
     plainEdit.insertPlainText("\n");
-    plainEdit.insertPlainText("<img src=\"C:\\Users\\hp\\Desktop\\D.T.png\" />");
+    plainEdit.insertPlainText("<img src=\":/res/D.T.png\" />");
 
+    /* QTextEdit */
     textEdit.move(20, 270);
     textEdit.resize(560, 130);
+#if 0
     textEdit.insertPlainText("QTextEdit");
-    textEdit.insertPlainText("\n"); // 在 html 中 "\n" 不代表换行
-    //textEdit.insertPlainText("<img src=\"C:\\Users\\hp\\Desktop\\D.T.png\" />");
+    textEdit.insertPlainText("\n");
+    textEdit.insertPlainText("<img src=\":/res/D.T.png\" />");
+#else
+    textEdit.insertHtml("QTextEdit");
+    textEdit.insertHtml("<br>"); /* 在 html 中 "\n" 不代表换行 */
     textEdit.insertHtml("<img src=\":/res/D.T.png\" />");
+#endif
 }
 
 MainWindow::~MainWindow()
